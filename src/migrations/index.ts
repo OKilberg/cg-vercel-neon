aa968329b83f60399fc7488ed22aeab4563c86ad
OKilberg <oscar.oravakilberg@gmail.com>
2026-03-08 17:51:14 +0100

    fix: generate migration files for new postgres instance

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260308_165015 from './20260308_165015';
+
+export const migrations = [
+  {
+    up: migration_20260308_165015.up,
+    down: migration_20260308_165015.down,
+    name: '20260308_165015'
+  },
+];
