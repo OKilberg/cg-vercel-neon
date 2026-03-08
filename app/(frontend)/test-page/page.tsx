113b45fc36f26109814fc11e836e3dcf061f4f7c
OKilberg <oscar.oravakilberg@gmail.com>
2026-03-08 17:42:45 +0100

    fix: avoid build error when page has not been created

--- app/(frontend)/test-page/page.tsx
+++ app/(frontend)/test-page/page.tsx
@@ -1,21 +1,29 @@
 import { getPayload } from "payload";
 import config from "@payload-config";
 import Block from "@/src/features/block/Block";
+import { notFound } from "next/navigation";
 
 const payload = await getPayload({ config });
 
 const TestPage = async () => {
-  const result = await payload.find({
-    collection: "pages",
-    where: {
-      "page-route": {
-        equals: "test-page",
+  const result = await payload
+    .find({
+      collection: "pages",
+      where: {
+        "page-route": {
+          equals: "test-page",
+        },
       },
-    },
-  });
+    })
+    .catch(() => ({ docs: [] }));
 
   const { docs = [] } = result;
   const [page] = docs;
+
+  if (!page) {
+    return notFound();
+  }
+
   const { sections = [] } = page;
 
   return (
